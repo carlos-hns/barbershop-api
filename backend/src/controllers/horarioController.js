6a4d4290807bc3eb85b44d3432e3c2f74d6ceff5
Carlos Henrique <carloshenriquend.santos@gmail.com>
2020-05-03 19:50:42 -0300

    CRUD de horario concluido

--- backend/src/controllers/horarioController.js
+++ backend/src/controllers/horarioController.js
@@ -17,5 +17,32 @@ router.get("/", async (req, res) => {
     return res.status(200).json(horarios.body);
 });
 
+router.post("/", async (req, res) => {
+
+    var { horario } = req.body;
+
+    //console.log(await Horario.findOne({where: {horario}, raw: true}));
+
+    if (await Horario.findOne({where: {horario}})){
+        return res.status(400).send("Horário já cadastrado");
+    }
+
+    await Horario.create(req.body);
+
+    return res.status(200).send("Horário cadastrado com sucesso");
+});
+
+router.delete("/:id", async(req,res) => {
+    
+    var id = req.params.id;
+
+    if (!await Horario.findOne({where: {id}})){
+        return res.status(400).send("Horário não cadastrado");
+    }
+
+    await Horario.destroy({where: {id}});
+
+    return res.status(200).send("Horário removido com sucesso");
+});
 
 module.exports = app => app.use("/horarios", router);
